df461b1f0173a8ba29775db37fbad573407ecf67
dyeun96 <dyeun96@naver.com>
2014-10-02 16:50:48 +0900

    g

--- sketch.js
+++ sketch.js
@@ -1,24 +1,53 @@
 function setup()
  {
   createCanvas(800, 600);
-  noFill();
   stroke(255);
 }
 
   function draw()
 {
   background(148,212,255);
+  	fill(255,255,255);
 if (mouseY < 100) {
-  ellipse(50, 200, 20, 20);
+	
+  ellipse(200, 50, 20, 20);
 } else if (mouseY < 200){
-  ellipse(150, 200, 50, 50);
+  ellipse(200, 150, 50, 50);
 } else if (mouseY < 300){
-  ellipse(250, 200, 80, 80);
+  ellipse(200, 250, 80, 80);
 } else if (mouseY < 400){
-  ellipse(350, 200, 100, 100);
+  ellipse(200, 350, 100, 100);
 } else if (mouseY < 500){
-  ellipse(450, 200, 50, 50);
+  ellipse(200, 450, 50, 50);
 } else {
-  ellipse(550, 200, 20, 20);
+  ellipse(200, 550, 20, 20);
+}
+ if (mouseY < 5) {
+
+  ellipse(500, 50, 20, 20);
+} else if (mouseY < 200){
+  ellipse(500, 150, 50, 50);
+} else if (mouseY < 300){
+  ellipse(500, 250, 80, 80);
+} else if (mouseY < 400){
+  ellipse(500, 350, 100, 100);
+} else if (mouseY < 500){
+  ellipse(500, 450, 50, 50);
+} else {
+  ellipse(500, 550, 20, 20);
+}
+ if (mouseY < 180) {
+
+  ellipse(700, 50, 20, 20);
+} else if (mouseY < 200){
+  ellipse(700, 150, 50, 50);
+} else if (mouseY < 300){
+  ellipse(700, 250, 80, 80);
+} else if (mouseY < 400){
+  ellipse(700, 350, 100, 100);
+} else if (mouseY < 500){
+  ellipse(700, 450, 50, 50);
+} else {
+  ellipse(700, 550, 20, 20);
 }
 }
