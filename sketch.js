2f455a3986b9d3aa9f9dc028cd4fc6169759c3c0
dyeun96 <dyeun96@naver.com>
2014-10-02 16:23:36 +0900

    d

--- sketch.js
+++ sketch.js
@@ -2,13 +2,15 @@ function setup()
  {
   createCanvas(800, 600);
   noFill();
-  stroke(255, 0, 0);
+  stroke(255);
 }
 
   function draw()
 {
-  background(0);
+  background(148,212,255);
   if (mouseX < 300) {
   ellipse(200,200,100,100);
+} else {
+  ellipse(400, 200, 100, 100);
 }
 }
