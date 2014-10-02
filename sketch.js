2da668b9294e646f4b0138e5b298747ee92e4c66
dyeun96 <dyeun96@naver.com>
2014-10-02 16:38:16 +0900

    F

--- sketch.js
+++ sketch.js
@@ -8,17 +8,17 @@ function setup()
   function draw()
 {
   background(148,212,255);
-if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
-} else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
-} else if (mouseX < 300){
-  ellipse(250, 200, 100, 100);
-} else if (mouseX < 400){
+if (mouseY < 100) {
+  ellipse(50, 200, 20, 20);
+} else if (mouseY < 200){
+  ellipse(150, 200, 50, 50);
+} else if (mouseY < 300){
+  ellipse(250, 200, 80, 80);
+} else if (mouseY < 400){
   ellipse(350, 200, 100, 100);
-} else if (mouseX < 500){
-  ellipse(450, 200, 100, 100);
+} else if (mouseY < 500){
+  ellipse(450, 200, 50, 50);
 } else {
-  ellipse(550, 200, 100, 100);
+  ellipse(550, 200, 20, 20);
 }
 }
